6a025452ead09881358ef5f2db2786b2a6c9ac60
David Heisel <47954555+dmheisel@users.noreply.github.com>
2019-03-06 09:32:58 -0600

    Update Morse_Code_1.py

--- Morse_Code_1.py
+++ Morse_Code_1.py
@@ -1,24 +1,30 @@
 #Codewars Morse Code decoder part 1
 #Codewars includes a Morse Code dictionary
+from itertools import groupby
 
 def decodeMorse(morse_code):    
-    code_letters = map(lambda x: MORSE_CODE.get(x) if MORSE_CODE.get(x) else ' ',[letter for letter in morse_code.split(' ')])
-    decoded = " ".join((''.join(code_letters)).split()) #joins all letters together then strips superfluous spaces
-    return decoded
-    
-    
-from itertools import groupby
+  morse_words = (morse_code.split(' '))
+  #iterates through list of each letter, if no letter inputs a space, then joins it together in a string for the decoded message
+  decoded = ''.join(map(lambda x: MORSE_CODE.get(x) if MORSE_CODE.get(x) else ' ',[letter for letter in morse_words]))
+  return decoded
+
 def decodeBits(bits):
-  bits_list = [''.join(g) for k, g in groupby(bits)]  
-  print(bits_list)
-  morse_code = ''
+  #converts string of bits into list of each input (groupby creates new string on each change between 0 and 1), 
+  #removes  any leading or trailing 0's
+  bits_list = [''.join(g) for k, g in groupby(bits.strip('0'))]  
+  #time unit is the minimum length of a group -- the smallest input is the base time unit.  *3 for dashes and letter-breaks, 
+  # * 7 for spaces
+  time_unit = min([len(bit) for bit in bits_list)
+  morse_code = '' 
   for bit in bits_list:
-    if int(bit[0]) == 0 and len(bit) % 7 == 0:
+    if int(bit[0]) == 0 and len(bit) == time_unit*3:
       morse_code += ' '
+    if int(bit[0]) == 0  and len(bit) == time_unit*7:
+      morse_code += '  '   
     if int(bit[0]) == 1:
-      if len(bit) % 3 == 0:
+      if len(bit) == time_unit*3:
         morse_code += '-'
       else:
         morse_code += '.'
-  print(morse_code)
-    
+  return morse_code
+
